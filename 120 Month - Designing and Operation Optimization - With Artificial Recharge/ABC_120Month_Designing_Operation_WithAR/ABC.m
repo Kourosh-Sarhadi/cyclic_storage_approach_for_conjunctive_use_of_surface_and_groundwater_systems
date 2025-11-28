96a71f7c5037293cc0d79b009d3123851ff2956f
Kourosh Sarhadi <kourosh.sarhadi.1994@gmail.com>
2025-11-28 15:03:47 +0330

    Add files via upload

---
+++ 120 Month - Designing and Operation Optimization - With Artificial Recharge/ABC_120Month_Designing_Operation_WithAR/ABC.m
@@ -0,0 +1,193 @@
+clc
+clear
+close all
+format shortG
+
+%% parameters setting
+
+lb=readmatrix('LB & UB 120 Month.xlsx','Sheet',1,'Range','B10:AAX10');  % Lower Bound of Variables
+ub=readmatrix('LB & UB 120 Month.xlsx','Sheet',1,'Range','B11:AAX11'); % Upper Bound of Variables
+
+nvar=725;                 % number of variable
+
+
+NB=200;                  % number of bee
+
+SN=2*round(NB/4);          % source number
+
+N_unlooker=NB-SN;        % unlooker number
+
+MCN=4000;                 % max of cycle number
+
+Trial=zeros(SN,1);
+
+Limit=1000000;   
+
+ndim=3;
+
+%% initialization
+tic
+
+emp.x=[];
+emp.fit=[];
+
+food=repmat(emp,SN,1);
+
+
+for i=1:SN
+
+food(i).x=lb+rand(1,nvar).*(ub-lb);
+food(i).fit=fitness(food(i).x);
+  
+end
+
+
+[value,index]=min([food(i).fit]);
+
+gfood=food(index);               % global food
+
+%% main loop
+
+best=zeros(MCN,1);
+AVR=zeros(MCN,1);
+
+for cycle=1:MCN
+
+    
+    % employed bee section
+    
+    for i=1:SN
+        
+        k=randsample([1:i-1  i+1:SN],1);
+        
+
+        
+        XK=food(k).x; % neighbor
+        
+        j=randsample(nvar,ndim)';
+        
+        X=food(i).x;
+        
+        NEWX=X;
+        
+        NEWX(j)=X(j)+unifrnd(-1,1,size(j)).*(X(j)-XK(j));
+        
+        NEWX=max(NEWX,lb);
+        NEWX=min(NEWX,ub);
+        
+        NEWFIT=fitness(NEWX);
+        
+        if NEWFIT<food(i).fit
+            
+            food(i).fit=NEWFIT;
+            food(i).x=NEWX;
+            Trial(i)=0;
+        else
+            Trial(i)=Trial(i)+1;
+        end
+        
+        
+        
+        
+        
+    end
+    
+    
+    
+    
+    
+    % unlooker bee section 
+    
+    
+ f=calculated_Pfit([food.fit]);
+
+ f=cumsum(f);
+ 
+ for n=1:N_unlooker
+          
+          i=RouletteWheel(f);
+     
+     
+        k=randsample([1:i-1  i+1:SN],1);
+        
+
+        
+        XK=food(k).x; % neighbor
+        
+        j=randsample(nvar,ndim)';
+        
+        X=food(i).x;
+        
+        NEWX=X;
+        
+        NEWX(j)=X(j)+unifrnd(-1,1,size(j)).*(X(j)-XK(j));
+        
+        NEWX=max(NEWX,lb);
+        NEWX=min(NEWX,ub);
+        
+        NEWFIT=fitness(NEWX);
+        
+        if NEWFIT<food(i).fit
+            
+            food(i).fit=NEWFIT;
+            food(i).x=NEWX;
+            Trial(i)=0;
+        else
+            Trial(i)=Trial(i)+1;
+        end
+  
+  end
+    
+    
+    
+    
+    
+    
+    % scout bee    section
+    
+    Q=find(Trial>Limit);
+    
+    for j=1:length(Q)
+        
+        i=Q(j);
+        food(i).x=lb+rand(1,nvar).*(ub-lb);
+        food(i).fit=fitness(food(i).x);
+        Trial(i)=0;
+    end
+    
+    
+   [value,index]=min([food.fit]);
+   
+   if value<gfood.fit
+       gfood=food(index);
+   end
+    
+  
+   best(cycle)=gfood.fit;
+   AVR(cycle)=mean([food.fit]);
+   
+   
+   disp([ '  Cycle = '  num2str(cycle)   '   BEST = '  num2str(best(cycle))])
+   
+end
+
+
+%% results
+writematrix(gfood.x,'Results_ABC_120Month_Designing_and_Operation_WithAR.xlsx','Sheet',1,'Range','B4:AAX4');
+%writematrix(best,'Best_ABC_Designing_Operation_WithoutAR.xlsx','Sheet',1,'Range','K2:K4001');
+disp('=====================================================')
+disp([ ' BEST solution  =  '  num2str(gfood.x)]);
+disp([ ' BEST fitness   =  '  num2str(gfood.fit)]);
+disp([ ' Time           =  '  num2str(toc)]);
+
+%figure(1)
+%plot(best(1:cycle),'r','LineWidth',2);
+%hold on
+%plot(AVR(1:cycle),'b','LineWidth',2);
+%xlabel('CYCLE')
+%ylabel('Fitness')
+%legend(' BEST', ' MEAN')
+%title ( 'ABC')
+
+
+
---
+++ 120 Month - Designing and Operation Optimization - With Artificial Recharge/GA_120Month_Designing_Operation_WithAR/GA.m
@@ -0,0 +1,115 @@
+
+clc
+clear
+close all
+%% paramters setting
+
+
+
+nvar=725;    % number of variable
+
+npop=50;         % number of population
+
+maxiter=5000;      % max of iteration
+
+pc=0.25;                  % percent of crossover
+
+ncross=2*round(npop*pc/2);   % number of cross over offspring
+
+pm=1-pc;                 % percent of mutation
+
+nmut=round(npop*pm);     % number of mutation offsprig
+
+lb=readmatrix('LB & UB 120 Month.xlsx','Sheet',1,'Range','B5:AAX5');  % Lower Bound of Variables
+ub=readmatrix('LB & UB 120 Month.xlsx','Sheet',1,'Range','B6:AAX6'); % Upper Bound of Variables
+
+%% initialization
+tic
+empty.par=[];
+empty.fit=[];
+
+
+pop=repmat(empty,npop,1);
+
+
+for i=1:npop
+    
+   pop(i).par=lb+rand(1,nvar).*(ub-lb);
+   pop(i).fit=fitness(pop(i).par);
+   
+end
+
+
+
+
+%% main loop
+
+
+BEST=zeros(maxiter,1);
+MEAN=zeros(maxiter,1);
+
+for iter=1:maxiter
+
+
+   % crossover
+   
+   crosspop=repmat(empty,ncross,1);
+   crosspop=crossover(crosspop,pop,ncross);
+   
+   
+   
+   
+   % mutation
+   mutpop=repmat(empty,nmut,1);
+   mutpop=mutation(mutpop,pop,nmut,npop,lb,ub,nvar);
+   
+   
+   % merged
+  [pop]=[pop;crosspop;mutpop];
+
+
+  % select
+  [value,index]=sort([pop.fit]);
+  pop=pop(index);
+  pop=pop(1:npop);
+
+ gpop=pop(1);   % global pop
+
+
+
+ BEST(iter)=gpop.fit;
+ MEAN(iter)=mean([pop.fit]);
+
+
+
+ disp([ ' Iter = '  num2str(iter)  '    BEST = '  num2str(BEST(iter))]);
+
+
+  if iter>150 && BEST(iter)==BEST(iter-150)
+      break
+  end
+
+end
+%% results
+writematrix(gpop.par,'Results_GA_120Month_Designing_and_Operation_WithAR.xlsx','Sheet',1,'Range','B4:AAX4');
+%writematrix(best,'Best_GA_Designing_Operation_WithoutAR.xlsx','Sheet',1,'Range','K2:K4001');
+disp(' ')
+disp([ ' Best par = '  num2str(gpop.par)])
+disp([ ' Best fitness = '  num2str(gpop.fit)])
+disp([ ' Time = '  num2str(toc)])
+
+
+
+%figure(1)
+%plot(BEST(1:iter),'r','LineWidth',2)
+%hold on
+%plot(MEAN(1:iter),'b','LineWidth',2)
+%xlabel('Iteration')
+%ylabel(' Fitness')
+%legend('BEST','MEAN')
+%title('GA for TSP')
+
+
+
+
+
